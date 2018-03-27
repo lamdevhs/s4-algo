3095e71d3f77564ee7d9799f99212c0f13fe8000
lamdevhs <lam.dev.hs@gmail.com>
2018-03-27 12:16:49 +0200

    divide/+backtracking/sudoku-tk.py

--- backtracking/sudoku-tk.py
+++ backtracking/sudoku-tk.py
@@ -10,10 +10,10 @@ def checkLine(solution, position):
   line = position // 9
   col = position % 9
   v = solution[position]
-  for i in range(col):
+  for i in range(9):
     ix = line*9 + i
     x = solution[ix]
-    if x == v:
+    if x == v and ix != position:
       return False
   return True
 
@@ -24,10 +24,10 @@ def checkColumn(solution, position):
   col = position % 9
   line = position // 9
   v = solution[position]
-  for i in range(line):
+  for i in range(9):
     ix = col + i*9
     x = solution[ix]
-    if x == v:
+    if x == v and ix != position:
       return False
   return True
 
@@ -41,18 +41,18 @@ def checkSquare(solution, position):
   v = solution[position]
   minLine = line - (line % 3)
   minCol = col - (col % 3)
-  for i in range(minLine, line):
+  for i in range(minLine, minLine + 3):
     for j in range(minCol, minCol + 3):
       ix = i*9 + j
       x = solution[ix]
-      if x == v:
+      if x == v and ix != position:
         return False
-  i = line
-  for j in range(minCol, col):
-    ix = i*9 + j
-    x = solution[ix]
-    if x == v:
-      return False
+  # i = line
+  # for j in range(minCol, minCol + 3): #col):
+  #   ix = i*9 + j
+  #   x = solution[ix]
+  #   if x == v:
+  #     return False
   return True
 
 # E: . liste représentant la solution explorée en cours
@@ -68,24 +68,29 @@ def checkNewAddition(solution, position):
 #    . valeur (nombre) à afficher
 #    . ligne du sudoku de la valeur
 #    . colonne du sudoku de la valeur
+#    . booléen qui indique si la valeur était dans la grille d'origine
 # S: affiche un nombre de la solution sur le canevas
-def printNumber(canvas, value, line, col):
+def printNumber(canvas, value, line, col, isInitial):
   value = str(value) if value != 0 else "."
   x = numberMargin + col*numberMargin*2 + squareMargin*(col // 3)
   y = numberMargin + line*numberMargin*2 + squareMargin*(line // 3)
   canvas.create_text(x, y, text = value,
     font = fontForPrinting,
-    fill = "black")
+    fill = "black" if not isInitial else "red")
 
 # E: . canevas sur lequel écrire l'avancement du backtracking
 #    . liste représentant la solution explorée en cours
+#    . liste d'origine, utilisée pour savoir quel chiffre
+#      était là à l'origine, pour les afficher différemment
 # S: affiche la solution en cours de construction
-def printToCanvas(canvas, solution):
+def printToCanvas(canvas, solution, initialGrid):
   canvas.delete(ALL)
   for line in range(9):
     for col in range(9):
-      value = solution[line*9 + col]
-      printNumber(canvas, value, line, col)
+      i = line*9 + col
+      value = solution[i]
+      isInitial = initialGrid[i] != 0
+      printNumber(canvas, value, line, col, isInitial)
   window.update()
 
 # E: . liste représentant la solution explorée en cours
@@ -106,41 +111,65 @@ def printToConsole(solution):
   print "#"*21
 
 # E: . liste représentant la solution explorée en cours
+#    . liste d'origine, utilisée pour savoir quel chiffre
+#      était là à l'origine, pour les afficher différemment
 #    . position de la dernière insertion dans la liste
 #    . canevas sur lequel écrire l'avancement du backtracking
 # S: effectue la résolution par backtracking,
 #    appelle les fonctions d'affichages quand nécessaire
-def backtracker(solution, position, canvas):
+#    retourne True pour casser le backtracking dès qu'une solution est
+#    trouvée
+def backtracker(solution, initialGrid, position, canvas):
   if 9*9 == position:
     printToConsole(solution)
     for _ in range(2): # blinking effect when solution found
-      printToCanvas(canvas, solution)
+      printToCanvas(canvas, solution, initialGrid)
       time.sleep(speed*5)
 
       canvas.delete(ALL)
       window.update()
       time.sleep(speed*5)
+    return True
 
   else:
-    printToCanvas(canvas, solution)
+    printToCanvas(canvas, solution, initialGrid)
     time.sleep(speed)
+    if solution[position] != 0: # we found a pre-filled position
+      backtracker(solution, initialGrid, position+1, canvas)
+    else:
+      for x in range(1, 9+1):
+        #print x, "x"
+        solution[position] = x
+        if checkNewAddition(solution, position):
+          stop = backtracker(solution, initialGrid, position+1, canvas)
+          if stop:
+            return stop
+        solution[position] = 0
+      return False # not stop
 
-    for x in range(1, 9+1):
-      solution[position] = x
-      if checkNewAddition(solution, position):
-        backtracker(solution, position+1, canvas)
-      solution[position] = 0
 
 # E: . canevas sur lequel écrire l'avancement du backtracking
 # S: prépare le backtracking, appelle le backtracker()
 def sudokuSolver(canvas):
-  solution = [0]*9*9
-  backtracker(solution, 0, canvas)
+  solution = [
+    0,0,0,  0,0,0,  0,0,8,
+    0,0,0,  0,0,6,  7,1,0,
+    0,6,0,  0,2,1,  0,0,0,
+
+    0,0,6,  5,0,0,  1,0,9,
+    0,0,9,  0,0,0,  3,0,0,
+    1,0,7,  0,0,4,  2,0,0,
+
+    0,0,0,  3,6,0,  0,5,0,
+    0,4,1,  7,0,0,  0,0,0,
+    8,0,0,  0,0,0,  0,0,0 ]
+  backtracker(solution, solution[:], 0, canvas)
+  print "done"
 
 
 # ------------------------------------
 
-speed = 0.1
+speed = 0.001
 fontForPrinting = ("Arial", 16)
 numberMargin = 15 # margin to separate columns / lines
 squareMargin = 10 # margin to separate each 3*3 square area
@@ -162,3 +191,16 @@ canvas.pack()
 sudokuSolver(canvas)
 
 window.mainloop()
+
+
+# votre grille
+  # solution = [
+  #   0, 8, 7, 0, 0, 0, 5, 2, 0,
+  #   9, 1, 0, 5, 0, 2, 0, 4, 6,
+  #   2, 0, 0, 0, 0, 0, 0, 0, 7,
+  #   0, 9, 0, 0, 2, 0, 0, 1, 0,
+  #   0, 0, 0, 1, 0, 6, 0, 0, 0,
+  #   0, 4, 0, 0, 9, 0, 0, 8, 0,
+  #   6, 0, 0, 0, 0, 0, 0, 0, 3,
+  #   5, 7, 0, 3, 0, 1, 0, 6, 8,
+  #   0, 3, 8, 0, 0, 0, 9, 5, 0 ]
