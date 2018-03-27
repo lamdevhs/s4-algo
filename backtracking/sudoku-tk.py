85fe96066aff9589c9dc2d156ef93b70fc029f04
lamdevhs <lam.dev.hs@gmail.com>
2018-03-27 17:04:30 +0200

    sublime.sh//sudoku-tk

--- backtracking/sudoku-tk.py
+++ backtracking/sudoku-tk.py
@@ -47,12 +47,6 @@ def checkSquare(solution, position):
       x = solution[ix]
       if x == v and ix != position:
         return False
-  # i = line
-  # for j in range(minCol, minCol + 3): #col):
-  #   ix = i*9 + j
-  #   x = solution[ix]
-  #   if x == v:
-  #     return False
   return True
 
 # E: . liste représentant la solution explorée en cours
@@ -71,12 +65,12 @@ def checkNewAddition(solution, position):
 #    . booléen qui indique si la valeur était dans la grille d'origine
 # S: affiche un nombre de la solution sur le canevas
 def printNumber(canvas, value, line, col, isInitial):
-  value = str(value) if value != 0 else "."
-  x = numberMargin + col*numberMargin*2 + squareMargin*(col // 3)
-  y = numberMargin + line*numberMargin*2 + squareMargin*(line // 3)
+  value = str(value) if value != 0 else " "
+  x = unitSize + col*unitSize*2
+  y = unitSize + line*unitSize*2
   canvas.create_text(x, y, text = value,
     font = fontForPrinting,
-    fill = "black" if not isInitial else "red")
+    fill = "black" if isInitial else "blue")
 
 # E: . canevas sur lequel écrire l'avancement du backtracking
 #    . liste représentant la solution explorée en cours
@@ -91,12 +85,18 @@ def printToCanvas(canvas, solution, initialGrid):
       value = solution[i]
       isInitial = initialGrid[i] != 0
       printNumber(canvas, value, line, col, isInitial)
+
+  for i in range(8):
+    x = (i+1)*unitSize*2
+    y = (i+1)*unitSize*2
+    width = 2 if i in [2,5] else 1
+    canvas.create_line(x, 0, x, dimension, width = width)
+    canvas.create_line(0, y, dimension, y, width = width)
   window.update()
 
 # E: . liste représentant la solution explorée en cours
 # S: affiche la solution finale valide dans le terminal
 def printToConsole(solution):
-  print
   print "#"*21
   for line in range(9):
     for col in range(9):
@@ -105,7 +105,7 @@ def printToConsole(solution):
 
       if col == 2 or col == 5:
         print "|", # square group separator
-    print # newline
+    print # newline needed
     if line == 2 or line == 5:
       print "-"*21
   print "#"*21
@@ -122,23 +122,27 @@ def printToConsole(solution):
 def backtracker(solution, initialGrid, position, canvas):
   if 9*9 == position:
     printToConsole(solution)
-    for _ in range(2): # blinking effect when solution found
+    for _ in range(3): # blinking effect when solution found
       printToCanvas(canvas, solution, initialGrid)
-      time.sleep(speed*5)
+      time.sleep(0.5)
 
       canvas.delete(ALL)
       window.update()
-      time.sleep(speed*5)
-    return True
+      time.sleep(0.5)
+    printToCanvas(canvas, solution, initialGrid)
+    return True # break the backtracking
 
   else:
-    printToCanvas(canvas, solution, initialGrid)
-    time.sleep(speed)
     if solution[position] != 0: # we found a pre-filled position
-      backtracker(solution, initialGrid, position+1, canvas)
+      stop = backtracker(solution, initialGrid, position+1, canvas)
+      if stop:
+        return stop
     else:
+      if doPrintProgress:
+        printToCanvas(canvas, solution, initialGrid)
+        time.sleep(speed)
+
       for x in range(1, 9+1):
-        #print x, "x"
         solution[position] = x
         if checkNewAddition(solution, position):
           stop = backtracker(solution, initialGrid, position+1, canvas)
@@ -151,29 +155,52 @@ def backtracker(solution, initialGrid, position, canvas):
 # E: . canevas sur lequel écrire l'avancement du backtracking
 # S: prépare le backtracking, appelle le backtracker()
 def sudokuSolver(canvas):
+  # solution = [
+  #   0,0,0,  0,0,0,  0,0,8,
+  #   0,0,0,  0,0,6,  7,1,0,
+  #   0,6,0,  0,2,1,  0,0,0,
+
+  #   0,0,6,  5,0,0,  1,0,9,
+  #   0,0,9,  0,0,0,  3,0,0,
+  #   1,0,7,  0,0,4,  2,0,0,
+
+  #   0,0,0,  3,6,0,  0,5,0,
+  #   0,4,1,  7,0,0,  0,0,0,
+  #   8,0,0,  0,0,0,  0,0,0 ]
   solution = [
-    0,0,0,  0,0,0,  0,0,8,
-    0,0,0,  0,0,6,  7,1,0,
-    0,6,0,  0,2,1,  0,0,0,
+    0,8,7, 0,0,0, 5,2,0,
+    9,1,0, 5,0,2, 0,4,6,
+    2,0,0, 0,0,0, 0,0,7,
 
-    0,0,6,  5,0,0,  1,0,9,
-    0,0,9,  0,0,0,  3,0,0,
-    1,0,7,  0,0,4,  2,0,0,
+    0,9,0, 0,2,0, 0,1,0,
+    0,0,0, 1,0,6, 0,0,0,
+    0,4,0, 0,9,0, 0,8,0,
 
-    0,0,0,  3,6,0,  0,5,0,
-    0,4,1,  7,0,0,  0,0,0,
-    8,0,0,  0,0,0,  0,0,0 ]
+    6,0,0, 0,0,0, 0,0,3,
+    5,7,0, 3,0,1, 0,6,8,
+    0,3,8, 0,0,0, 9,5,0 ]
   backtracker(solution, solution[:], 0, canvas)
   print "done"
 
 
+
+
 # ------------------------------------
 
-speed = 0.001
+speed = 0.2
 fontForPrinting = ("Arial", 16)
-numberMargin = 15 # margin to separate columns / lines
-squareMargin = 10 # margin to separate each 3*3 square area
-# ^ also used in printNumber()
+unitSize = 15 # unité de base pour la grille de sudoku
+# ^ chaque nombre est dans un petit carré de la grille de côté unitSize*2
+# ^ surtout utile pour printNumber(), et pour calculer `dimension`
+
+dimension = unitSize*2*9
+# ^ taille (verticale et horizontale) du canevas
+doPrintProgress = True
+# ^ afficher la progression
+# si False, n'affiche pas le backtracking dans tk,
+# juste la réponse finale trouvée
+
+# ~~~~~ ~~~~~ ~~~~~
 
 window = Tk() 
 frame = Frame(window, borderwidth = 4)
@@ -182,25 +209,9 @@ label = Label(window, text = "Sudoku Solver", fg = "black",
   font = fontForPrinting)
 label.pack(side = "top")
 
-
-dimension = numberMargin*18 + 2*squareMargin
-
 canvas = Canvas(window, height = dimension, width = dimension, bg = "white")
 canvas.pack()
 
 sudokuSolver(canvas)
 
 window.mainloop()
-
-
-# votre grille
-  # solution = [
-  #   0, 8, 7, 0, 0, 0, 5, 2, 0,
-  #   9, 1, 0, 5, 0, 2, 0, 4, 6,
-  #   2, 0, 0, 0, 0, 0, 0, 0, 7,
-  #   0, 9, 0, 0, 2, 0, 0, 1, 0,
-  #   0, 0, 0, 1, 0, 6, 0, 0, 0,
-  #   0, 4, 0, 0, 9, 0, 0, 8, 0,
-  #   6, 0, 0, 0, 0, 0, 0, 0, 3,
-  #   5, 7, 0, 3, 0, 1, 0, 6, 8,
-  #   0, 3, 8, 0, 0, 0, 9, 5, 0 ]
